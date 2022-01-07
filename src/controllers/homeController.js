5e2d054d8b61ade75218fb855fa9babf713f2fad
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-01-07 16:14:48 +0700

    VIDEO 26, 27: Hiển thị dữ liệu, sửa dữ liệu thành công ORM

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -22,8 +22,40 @@ let postCrud = async (req, res) => {
     return res.send('post ok');
 };
 
+let getDisplayCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUsers();
+    return res.render('displayCRUD', {
+        dataTable: data,
+    });
+};
+
+let getEditCrud = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render('edit-Crud', {
+            dataUser: userData,
+        });
+    } else {
+        return res.send('with id FAILED ');
+    }
+};
+
+let putUserCrud = async (req, res) => {
+    let data = req.body;
+    let newUser = await CRUDService.updateUserData(data);
+
+    return res.render('displayCRUD', {
+        dataTable: newUser,
+    });
+};
+
 module.exports = {
     homeController,
     getCrud,
     postCrud,
+    getDisplayCRUD,
+    getEditCrud,
+    putUserCrud,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -10,4 +10,10 @@ router.get('/crud', homeController.getCrud);
 
 router.post('/post-crud', homeController.postCrud);
 
+router.get('/get-crud', homeController.getDisplayCRUD);
+
+router.get('/edit-crud', homeController.getEditCrud);
+
+router.post('/put-crud', homeController.putUserCrud);
+
 module.exports = router;
